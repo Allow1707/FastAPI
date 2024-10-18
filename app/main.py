43af27ba4f23f64bd88653f515a6cf8e32582557
Алексей <alex@stargaze.ru>
2024-10-18 11:52:31 +0300

    2024-10-17: feat: module 3

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 import json
 
-from fastapi import FastAPI
+from typing import Annotated, Dict
+from fastapi import FastAPI, Header, Request
+from fastapi.responses import JSONResponse
+from pygments.lexer import default
+
 from app.models.user import User
 from app.models.calculate import CalculateBody
 from app.models.feedback import Feedback
@@ -58,3 +62,72 @@ async def get_feedback_root(limit: int = 10):
         feedback_data: dict = json.loads(f.read())
     content = dict(list(feedback_data.items())[:limit])
     return content
+
+
+@app.post(f"/create_user")
+async def create_user_root(body: User) -> User:
+    with open(f"app/db/user.json", "r", encoding="utf-8") as f:
+        feedback_data: dict = json.loads(f.read())
+    index: int = body.id
+    feedback_data.update({f"{index}": dict(body)})
+    with open(f"app/db/user.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(feedback_data))
+    content = dict(body)
+    User(**content)
+    return User(**content)
+
+def read_jsonl(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            yield json.loads(line.strip())
+
+
+
+@app.get("/get_product/{product_id}")
+async def get_product_root(product_id: int) -> dict:
+    content: dict = dict()
+    for item in read_jsonl('app/db/products.jsonl'):
+        if item['product_id'] == product_id:
+            content = item
+            break
+    return content
+
+
+def check_item_by_filter(item: dict, keyword: str, category: str | None) -> bool:
+    content = True
+    if keyword not in item['name'].lower():
+        content = False
+    else:
+        if category:
+            content = item['category'] == category
+    return content
+
+
+@app.get("/find_product")
+async def get_product_root(keyword: str, category: str | None = None, limit: int | None = None) -> list:
+    content: list = list()
+    for item in read_jsonl('app/db/products.jsonl'):
+        if check_item_by_filter(item, keyword, category):
+            if limit:
+                if len(content) < limit:
+                    content.append(item)
+                else:
+                    break
+            else:
+                content.append(item)
+
+    return content
+
+@app.get("/headers")
+async def get_product_root(request: Request):
+    try:
+        user_agent: str = request.headers["user-agent"]
+        accept_language: str = request.headers["accept-language"]
+    except KeyError as e:
+        return JSONResponse(content={"message": "BadRequest"}, status_code=400)
+    return {
+        "User-Agent": user_agent,
+        "Accept-Language": accept_language
+    }
+
+
--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
 
 class User(BaseModel):
+    """
     id: int
     name: str
     age: int
+    """
+    id: int
+    name: str
+    age: int
+    is_subscription: bool = False
